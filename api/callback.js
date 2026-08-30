ce726ad052eac462bb65400c6efc1dcfe40ef2a3
Mahmoud Haj ali <mahmoud.al7aj.ali@gmail.com>
2026-08-30 15:09:24 +0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   const params = new URLSearchParams(req.query).toString();
-  res.redirect(`http://localhost:3000/webhook/callback?${params}`);
+  res.redirect(`http://localhost:4200?${params}`);
 }
