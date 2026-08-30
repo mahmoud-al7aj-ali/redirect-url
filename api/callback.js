f5100a0c5e5731f4739bf0be233a0fb95fbc5b68
Mahmoud Haj ali <mahmoud.al7aj.ali@gmail.com>
2026-08-30 16:10:43 +0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   const params = new URLSearchParams(req.query).toString();
-  res.redirect(`http://localhost:4200?${params}`);
+  res.redirect(`http://localhost:4200/payment-callback?${params}`);
 }
